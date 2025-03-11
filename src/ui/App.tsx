55446ede059a949f46bf33ca9cd2c70608be0154
gillsB <78826213+gillsB@users.noreply.github.com>
2025-03-10 18:54:52 -0700

    Cleanup, main chart only.
    Removed most .css and react code auto generated by workspace.

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -1,11 +1,9 @@
 import { useEffect, useMemo, useState } from "react";
 import "./App.css";
-import reactLogo from "./assets/react.svg";
 import { Chart } from "./Chart";
 import { useStatistics } from "./useStatistics";
 
 function App() {
-  const [count, setCount] = useState(0);
   const statistics = useStatistics(10);
   const [activeView, setActiveView] = useState<View>("CPU");
   const cpuUsages = useMemo(
@@ -42,48 +40,39 @@ function App() {
   return (
     <>
       <div className="App">
-        <header>
-          <button
-            id="minimize"
-            onClick={() => window.electron.sendFrameAction("MINIMIZE")}
-          >
-            ─
-          </button>
-          <button
-            id="maximize"
-            onClick={() => window.electron.sendFrameAction("MAXIMIZE")}
-          >
-            <span className="maximize-icon"></span>
-          </button>
-          <button
-            id="close"
-            onClick={() => window.electron.sendFrameAction("CLOSE")}
-          >
-            ✕
-          </button>
-        </header>
-        <div style={{ height: 120 }}>
-          <Chart data={activeUsages} maxDataPoints={10}></Chart>
-        </div>
+        <Header />
         <div>
-          <a href="https://react.dev" target="_blank" rel="noopener noreferrer">
-            <img src={reactLogo} className="logo react" alt="React logo" />
-          </a>
+          <div className="mainGrid">
+            <Chart data={activeUsages} maxDataPoints={10}></Chart>
+          </div>
         </div>
-        <h1>Vite + React</h1>
-        <div className="card">
-          <button onClick={() => setCount((count) => count + 1)}>
-            count is {count}
-          </button>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test HMR
-          </p>
-        </div>
-        <p className="read-the-docs">
-          Click on the Vite and React logos to learn more
-        </p>
       </div>
     </>
+  );
+}
+
+function Header() {
+  return (
+    <header>
+      <button
+        id="minimize"
+        onClick={() => window.electron.sendFrameAction("MINIMIZE")}
+      >
+        ─
+      </button>
+      <button
+        id="maximize"
+        onClick={() => window.electron.sendFrameAction("MAXIMIZE")}
+      >
+        <span className="maximize-icon"></span>
+      </button>
+      <button
+        id="close"
+        onClick={() => window.electron.sendFrameAction("CLOSE")}
+      >
+        ✕
+      </button>
+    </header>
   );
 }
 
